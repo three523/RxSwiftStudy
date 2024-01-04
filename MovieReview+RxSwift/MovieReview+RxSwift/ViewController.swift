9bff531e36a95210f13991f80a31cc54f23f42b4
dohyun <thtree523@gmail.com>
2024-01-04 19:43:28 +0900

    [Init] 초기 셋팅

---
+++ MovieReview+RxSwift/MovieReview+RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieReview+RxSwift
+//
+//  Created by 김도현 on 2024/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
